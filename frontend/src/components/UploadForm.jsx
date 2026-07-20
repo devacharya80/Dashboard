fa6a5d012016484e57e2ffcfd84b4797768c7fe6
devacharya80 <dhanudeva80@gmail.com>
2026-07-20 12:39:06 +0530

    Implement CSV upload interface

--- frontend/src/components/UploadForm.jsx
+++ frontend/src/components/UploadForm.jsx
@@ -1,8 +1,74 @@
+import { useState } from "react";
+import api from "../services/api";
+
 function UploadForm() {
+  const [ordersFile, setOrdersFile] = useState(null);
+  const [paymentsFile, setPaymentsFile] = useState(null);
+
+  const [loading, setLoading] = useState(false);
+
+  const handleUpload = async () => {
+    if (!ordersFile || !paymentsFile) {
+      alert("Please select both CSV files.");
+      return;
+    }
+
+    const formData = new FormData();
+
+    formData.append("orders", ordersFile);
+    formData.append("payments", paymentsFile);
+
+    try {
+      setLoading(true);
+
+      const { data } = await api.post("/upload", formData, {
+        headers: {
+          "Content-Type": "multipart/form-data",
+        },
+      });
+
+      alert(data.message);
+    } catch (error) {
+      alert(error.response?.data?.message || "Upload Failed");
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
-    <div className="card shadow-sm">
+    <div className="card shadow">
       <div className="card-body">
-        <h4>Upload CSV Files</h4>
+        <h4 className="mb-4">Upload CSV Files</h4>
+
+        <div className="mb-3">
+          <label className="form-label">Orders CSV</label>
+
+          <input
+            type="file"
+            accept=".csv"
+            className="form-control"
+            onChange={(e) => setOrdersFile(e.target.files[0])}
+          />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label">Payments CSV</label>
+
+          <input
+            type="file"
+            accept=".csv"
+            className="form-control"
+            onChange={(e) => setPaymentsFile(e.target.files[0])}
+          />
+        </div>
+
+        <button
+          className="btn btn-primary"
+          disabled={loading}
+          onClick={handleUpload}
+        >
+          {loading ? "Uploading..." : "Upload"}
+        </button>
       </div>
     </div>
   );
