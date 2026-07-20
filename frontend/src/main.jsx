cd958ceb36ea1cc09cb57bcd95f01d4dad3333d1
devacharya80 <dhanudeva80@gmail.com>
2026-07-20 13:27:43 +0530

    Add global dashboard styling

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,6 +8,8 @@ import { AuthProvider } from "./context/AuthContext";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+import "./style.css";
+
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
